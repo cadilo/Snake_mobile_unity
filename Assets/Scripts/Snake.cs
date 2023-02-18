3ae7c567fbde30c6d9cf0b7e834f423a73b1a30f
VitaliyShein <62501338+VitaliyShein@users.noreply.github.com>
2023-02-18 23:13:38 +0500

    v2

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -73,25 +73,50 @@ public class Snake : MonoBehaviourPunCallbacks
     }
     private void Update()
     {
-  
+
 
         //====================================== Управление клавиатурой
+        if (flagmultiplayer)
+        {
+            if (view.IsMine)
+            {
 
-        if(Input.GetKey(KeyCode.W)) 
-        {
-            direction = Vector2.up;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    direction = Vector2.up;
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    direction = Vector2.down;
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    direction = Vector2.left;
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    direction = Vector2.right;
+                }
+            }
         }
-        else if (Input.GetKey(KeyCode.S))
+        else 
         {
-            direction = Vector2.down;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            direction = Vector2.left;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            direction = Vector2.right;
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction = Vector2.up;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                direction = Vector2.down;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                direction = Vector2.left;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                direction = Vector2.right;
+            }
         }
         //==============================================================
 
